c51c96f2646fa93ffbab2cdf14c8bd3592fa6554
ericssonl07 <ljh07ericsson@gmail.com>
2025-02-18 18:15:09 +0800

    Separated selection into hpp and cpp; added logic for full control flow (selection, setting, and firing)

--- include/selection.hpp
+++ include/selection.hpp
@@ -4,59 +4,8 @@
 
 #include <vex.h>
 
-double selector(double max)
-{
-    vex::controller controller;
-    double value;
-    int power = 0;
-    bool lastL, lastR, lastU, lastD;
-    lastL = lastR = lastU = lastD = false;
-    while (!controller.ButtonA.pressing())
-    {
-        controller.Screen.clearScreen();
-        controller.Screen.setCursor(1, 1);
-        controller.Screen.print("%em", value);
-        // selection mode
-        if (controller.ButtonLeft.pressing() && !lastL)
-        {
-            // left was pressed
-            double new_value = value - pow(10, power);
-            if (new_value >= 0 and new_value <= max)
-            {
-                value = new_value;
-            } else
-            {
-                controller.rumble("..");
-            }
-        }
-        else if (controller.ButtonRight.pressing() && !lastR)
-        {
-            // right was pressed
-            double new_value = value + pow(10, power);
-            if (new_value >= 0 and new_value <= max)
-            {
-                value = new_value;
-            } else
-            {
-                controller.rumble("..");
-            }
-        }
-        else if (controller.ButtonUp.pressing() && !lastU)
-        {
-            // up was pressed
-            ++power;
-        }
-        else if (controller.ButtonDown.pressing() && !lastD)
-        {
-            // down was pressed
-            --power;
-        }
-        lastL = controller.ButtonLeft.pressing();
-        lastR = controller.ButtonRight.pressing();
-        lastU = controller.ButtonUp.pressing();
-        lastD = controller.ButtonDown.pressing();
-    }
-    return value;
-}
+extern vex::controller controller;
+
+double selector(double max);
 
 #endif // #ifndef SELECTION_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,30 @@
 /*----------------------------------------------------------------------------*/
 #include <vex.h>
 #include <motor.hpp>
+#include <selection.hpp>
 
 vex::brain brain;
+vex::controller controller;
+vex::pneumatics pneumatics(brain.ThreeWirePort.A);
+Motor motor(vex::PORT14, vex::gearSetting::ratio36_1, 1, false, Quantity(0.051, Unit::meter()));
 
 int main() {
-    Motor motor(vex::PORT14, vex::gearSetting::ratio36_1, 1, false, Quantity(0.051, Unit::meter()));
-    // motor.spin_angular(Quantity(2*M_PI, Unit::radian() / Unit::second()));
-    // motor.spin(Quantity(0.32044245, Unit::meter() / Unit::second()));
-    motor.spin_distance(Quantity(0.32044245, Unit::meter()));
-    while(true)vexDelay(1000);
+    const double max_meter_displacement = 0.3;
+    pneumatics.open();
+    double distance = selector(max_meter_displacement);
+    motor.spin_distance(distance, Unit::meter());
+    while (true)
+    {
+        if (controller.ButtonA.pressing())
+        {
+            pneumatics.close();
+            break;
+        }
+        vex::this_thread::sleep_for(20);
+    }
+    // motor.spin_distance(Quantity(0.32044245, Unit::meter())); // 2 inches * 2PI
+    while (true)
+    {
+        vexDelay(1000);
+    }
 }
